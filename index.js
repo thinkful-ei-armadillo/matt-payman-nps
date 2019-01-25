9f7d995fc3037eef629876b6467d95235590e443
Payman Kossari <Paymankossari@gmail.com>
2019-01-25 16:05:11 -0500

    nps api completed functions, edited css

--- index.js
+++ index.js
@@ -4,29 +4,71 @@ const API_KEY = 'beZfV6NTHQd21SZqUHZhVHwE9kWKeKDmFUl6gqvD';
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  const queryItems = Object.keys(params).map(
+    key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
   return queryItems.join('&');
+}
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the items array
+  let html = '';
+  for (let i = 0; i < responseJson.data.length; i++) {
+    const park = responseJson.data[i];
+    const description = park.description;
+    const name = park.name;
+    const url = park.url;
+    const address = park.directionsInfo;
+
+    html += `
+        <li><h3>${name}</h3>
+          <p>Description: ${description}</p>
+          <p>URL : <a href="${url}">${url}</a></p>
+          <p>Address: ${address}</p>
+        </li>`;
+  }
+  //display the results section
+  $('#results-list').html(html);
+  $('#results').removeClass('hidden');
 }
 
 function getParkResults(query, maxResults = 10) {
+  maxResults -= 1;
+
   const params = {
-    key: API_KEY,
-    stateCode: query,
+    api_key: API_KEY,
     limit: maxResults,
+    stateCode: query
   };
+
   const queryString = formatQueryParams(params);
-  const url = searchURL + '?' +queryString;
+  const url = searchURL + '?' + queryString;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
-
-
-
 function watchForm() {
-  $('form').submit(function (event) {
-    const state = $(this).find('#State').val();
-    const maxResults = $(this).find('#Max').val();
-
+  $('.user-form').submit(function(event) {
+    event.preventDefault();
+    const state = $(this)
+      .find('#state')
+      .val();
+    const maxResults = $(this)
+      .find('#max')
+      .val();
     getParkResults(state, maxResults);
   });
 }
+
+$(watchForm);
